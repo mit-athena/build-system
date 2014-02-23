b2e94798706d4c6a290c2a8372ce9e9ca50b6d2c
Victor Vasiliev <vasilvv@mit.edu>
2014-02-23 15:49:38 -0500

    Add functions to determine which packages need building

--- dabuildsys/apt.py
+++ dabuildsys/apt.py
@@ -21,7 +21,10 @@ from debian.debian_support import Version
 from collections import defaultdict
 import debian.deb822
 import glob
+import gzip
 import os.path
+import re
+import tarfile
 
 class APTFile(object):
     """A file in the APT repository."""
@@ -32,16 +35,115 @@ class APTFile(object):
         self.sha256 = sha256
 
 class APTSourcePackage(object):
-    def __init__(self, name, version, architecture):
+    def __init__(self, name, manifest):
         self.name = name
-        self.version = Version(version)
-        self.architecture = architecture
+        self.version = Version(manifest['Version'])
+        self.architecture = manifest['Architecture']
+        self.binaries = manifest['Binary'].split(', ')
+        self.manifest = manifest
+        self.format = manifest['Format']
 
     def __str__(self):
         return "%s=%s" % (self.name, self.version)
 
     def __repr__(self):
         return str(self)
+
+    def get_control_file(self):
+        """Extract control file from the source package."""
+
+        if self.format.startswith('3.0'):
+            if self.format == '3.0 (native)':
+                tarname = "%s_%s.tar." % (self.name, str(self.version))
+            elif self.format == '3.0 (quilt)':
+                tarname = "%s_%s.debian.tar." % (self.name, str(self.version))
+            else:
+                raise BuildError("Package %s has unsupported format %s in archive" % (self.name, self.format))
+
+            try:
+                tarpath, = [f.path for f in self.files if f.name.startswith(tarname)]
+            except:
+                raise BuildError("File %s.{gz,bz2,xz} not found for package %s" % (tarname, self.name))
+
+            with tarfile.open(tarpath, 'r:*') as tar:
+                return list(debian.deb822.Deb822.iter_paragraphs(
+                    tar.extractfile('debian/control')  ))
+
+        # FIXME: this code should be gone once 1.0 packages are gone
+        # I still can't believe I actually wrote this
+        elif self.format == '1.0':
+            if len(self.files) == 2:
+                try:
+                    tarpath, = [f.path for f in self.files if f.name.endswith('.tar.gz')]
+                except:
+                    raise BuildError("Package %s has format 1.0 and does not seem to have the tarball" % self.name)
+
+                controlname = "%s-%s/debian/control" % (self.name, str(self.version))
+                with tarfile.open(tarpath, 'r:*') as tar:
+                    return list(debian.deb822.Deb822.iter_paragraphs(
+                        tar.extractfile(controlname)  ))
+            else:
+                diffname = "%s_%s.diff.gz" % (self.name, str(self.version))
+                try:
+                    diffpath, = [f.path for f in self.files if f.name == diffname]
+                except:
+                    raise BuildError("File %s not found for package %s" % (diffname, self.name))
+
+                diff = gzip.open(diffpath, 'r')
+                while True:
+                    line = diff.readline().strip()
+                    if line.startswith('--- ') and line.endswith('/debian/control'):
+                        line = diff.readline().strip()
+                        if not line.startswith('+++ '):
+                            raise BuildError("Malformed debian diff in package " + self.name)
+
+                        line = diff.readline()
+                        match = re.match(r"@@ -0,0 \+1,(\d+) @@", line)
+                        if not match:
+                            raise BuildError("Malformed debian diff in package " + self.name)
+                        number_of_lines = int(match.group(1))
+
+                        lines = []
+                        for i in range(number_of_lines):
+                            lines.append(diff.readline()[1:])
+
+                        return list(debian.deb822.Deb822.iter_paragraphs(lines))
+        else:
+            raise BuildError("Package %s has unsupported format %s in archive" % (self.name, self.format))
+
+    def get_binary_architectures(self):
+        """Returns the dictionary of binary packages to list of architectures
+        for which those packages are built."""
+
+        # See commit 47126733bb in dpkg.
+        # Prior to May 15, 2011, dpkg did not output "Architecture: any all"
+        # for packages which contained both any and all architectures.
+        # Here I attempt to detect those buggy packages by asserting that
+        # packages with Package-List (which was finally introduced on May 28
+        # same year, though it kind of existed some time before).
+        dpkg_bug = 'Package-List' not in self.manifest
+
+        arches_naive = self.architecture.split(' ')
+        if len(self.binaries) == 1:
+            return { self.binaries[0] : arches_naive }
+        if not dpkg_bug and arches_naive == ['all']:
+            return { binary: arches_naive for binary in self.binaries }
+        # "any" is unsafe because some of the binaries may have more
+        # restrictive architectures
+
+        # Actually very limited number of packages goes here
+        control = self.get_control_file()
+        binaries = {}
+        for package in control:
+            if 'Source' in package:
+                continue
+
+            binaries[package['Package']] = package['Architecture'].split(' ')
+
+        if set(binaries) != set(self.binaries):
+            raise BuildError("Package %s has mismatching list of binaries in dsc and control file" % self.name)
+
+        return binaries
 
 class APTBinaryPackage(object):
     def __init__(self, name, version, architecture):
@@ -79,7 +181,7 @@ class APTDistribution(object):
         for sources_file_path in glob.glob(os.path.join(self.path, '*', 'source',  'Sources')):
             with open(sources_file_path, 'r') as sources_file:
                 for source_pkg in debian.deb822.Sources.iter_paragraphs(sources_file):
-                    pkg = APTSourcePackage(source_pkg['Package'], source_pkg['Version'], source_pkg['Architecture'])
+                    pkg = APTSourcePackage(source_pkg['Package'], source_pkg)
                     basedir = os.path.join(config.apt_root_dir, source_pkg['Directory'])
                     pkg.files = [APTFile(f['name'], basedir, f['sha256']) for f in source_pkg['Checksums-Sha256']]
                     self.sources[pkg.name] = pkg
@@ -113,6 +215,48 @@ class APTDistribution(object):
                 else:
                     self.binaries[name] = {}
                     self.binaries[name][arch] = pkg_other
+
+    def out_of_date_binaries(self, arch):
+        """Find all packages for which there is a source package in the
+        repository, but not a binary one for a given architecture.  Returns
+        a list of source packages which need rebuilding."""
+
+        result = []
+        for name, src_pkg in self.sources.iteritems():
+            out_of_date = False
+            for binary, arches in src_pkg.get_binary_architectures().iteritems():
+                # Handle cases when package is not meant to be built
+                # in the given architecture
+                if arch == 'all' and 'all' not in arches:
+                    continue
+                if arch != 'all' and not ('any' in arches or arch in arches):
+                    continue
+
+                # Package was never built
+                if binary not in self.binaries:
+                    out_of_date = True
+                    break
+
+                # Package was not built for this archictecture
+                bin_pkgs = self.binaries[binary]
+                if arch not in bin_pkgs:
+                    out_of_date = True
+                    break
+
+                # Actually compare versions
+                bin_pkg = bin_pkgs[arch]
+                if bin_pkg.version > src_pkg.version:
+                    # Circumvent edge cases of version comparison with manual-config packages
+                    if not (name.startswith('debathena-manual-') and name.endswith('-config')):
+                        raise BuildError("Package %s has version higher in binary than in source" % bin_pkg.name)
+                if src_pkg.version > bin_pkg.version:
+                    out_of_date = True
+                    continue
+
+            if out_of_date:
+                result.append(name)
+
+        return result
 
 def get_release(distribution):
     """For given release, returns (production, proposed, development)
